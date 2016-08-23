90efc7dff177a23f0735ee1addbdad2628a12fdf
Adam Sigel <adsigel@gmail.com>
2016-08-23 14:41:44 -0400

    updates to sign up

--- LoginController.swift
+++ LoginController.swift
@@ -19,6 +19,7 @@ import Firebase
 
 var userDict = [String:AnyObject]()
 var userID : String = ""
+var loggedIn = false
 
 class LoginController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate {
     
@@ -29,9 +30,64 @@ class LoginController: UIViewController, UITextFieldDelegate, UINavigationContro
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.checkForUserInitial()
     }
-     
+    
+    func checkForUserInitial() {
+        FIRAuth.auth()?.addAuthStateDidChangeListener { auth, user in
+            if let user = user {
+                // user is signed in
+                print("A user exists.")
+                loggedIn = true
+                print("loggedIn = \(loggedIn)")
+                uid = user.uid
+                print("Fetching user data from user child path \(uid)")
+                self.currentUser(uid)
+                print("Here is userDict: \(userDict)")
+            } else {
+                //no user is signed in
+                loggedIn = false
+                print("loggedIn = \(loggedIn)")
+                
+            }
+        }
+    }
+
+    
+    func currentDate() {
+        let dateformatter = NSDateFormatter()
+        dateformatter.dateStyle = NSDateFormatterStyle.LongStyle
+        dateformatter.timeStyle = NSDateFormatterStyle.LongStyle
+        dateString = dateformatter.stringFromDate(NSDate())
+    }
+
+    func currentUser(uid:String) {
+        print("Current user is signed in with uid \(uid)")
+        let currentUserRef = userRef.ref.child(uid)
+        print("currentUserRef is \(currentUserRef)")
+        currentUserRef.observeEventType(.Value, withBlock: { (snapshot) in
+            userDict = snapshot.value as! [String : AnyObject]
+            print("userDict is \(userDict)")
+        })
+    }
+    
+    func addNewUser() {
+        self.currentDate()
+        let userData = User(uid: userDict["uid"]! as! String, email: userDict["email"]! as! String, displayName: userDict["name"]! as! String, score: 0, addedDate: dateString)
+        let userRef = ref.child("users/")
+        let newUser = userRef.child(uid)
+        // TODO: MAKE SURE NEW CHILD NODE ADDED FOR NEW USER
+        newUser.setValue(userData.toAnyObjectUser())
+        newUser.child("exclude").child("key").setValue(true)
+        print("New user added with uid of \(uid) and email \(userDict["email"])")
+    }
+    
+    @IBAction func checkUserEmail(sender: AnyObject) {
+        if let user = FIRAuth.auth()?.currentUser {
+            let email = user.email
+        } else {
+            // No user is signed in.
+        }
+    }
     
     @IBAction func signUpButton(sender: AnyObject) {
         let email = emailTextField.text?.lowercaseString.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
@@ -48,7 +104,7 @@ class LoginController: UIViewController, UITextFieldDelegate, UINavigationContro
                         name = ""
                     }
                     let email = user!.email!
-                    userDict = ["uid": uid, "name": name, "email": email]
+                    userDict = ["uid": uid, "name": name, "email": email, "score": 0, "exclude": ["key": true]]
                     self.addNewUser()
                     FIRAuth.auth()?.signInWithEmail(email, password: password!) { (user, error) in
                     }
@@ -61,90 +117,71 @@ class LoginController: UIViewController, UITextFieldDelegate, UINavigationContro
     
     
     @IBAction func logInButton(sender: AnyObject) {
-        FIRAuth.auth()?.addAuthStateDidChangeListener { auth, user in
-            if let user = user {
-                // user is signed in
-                print("A user exists.")
-                uid = user.uid
-                print("Fetching user data from user child path \(uid)")
-                self.currentUser(uid)
-                self.performSegueWithIdentifier("logIn", sender: sender)
-            }
-            else {
-                print("No current user.")
-                let anim = CAKeyframeAnimation( keyPath:"transform" )
-                anim.values = [
-                    NSValue( CATransform3D:CATransform3DMakeTranslation(-10, 0, 0 ) ),
-                    NSValue( CATransform3D:CATransform3DMakeTranslation( 10, 0, 0 ) )
-                ]
-                anim.autoreverses = true
-                anim.repeatCount = 2
-                anim.duration = 7/100
-                
-                self.passwordTextField.layer.addAnimation( anim, forKey:nil )
-                self.welcomeTextLabel.hidden = false
-                self.welcomeTextLabel.text = "Please sign in first"
-            }}
-    }
-   
-
-    func currentDate() {
-        let dateformatter = NSDateFormatter()
-        dateformatter.dateStyle = NSDateFormatterStyle.LongStyle
-        dateformatter.timeStyle = NSDateFormatterStyle.LongStyle
-        dateString = dateformatter.stringFromDate(NSDate())
-    }
-
-    
-    func currentUser(uid:String) {
-        print("Current user is signed in with uid \(uid)")
-        let currentUserRef = userRef.ref.child(uid)
-        print("currentUserRef is \(currentUserRef)")
-        currentUserRef.observeEventType(.Value, withBlock: { (snapshot) in
-            userDict = snapshot.value as! [String : AnyObject]
-            print("userDict is \(userDict)")
-            })
-    }
-
-    func checkForUserInitial() {
-        FIRAuth.auth()?.addAuthStateDidChangeListener { auth, user in
-            if let user = user {
-                // user is signed in
-                print("A user exists.")
-                uid = user.uid
-                print("Fetching user data from user child path \(uid)")
-                self.currentUser(uid)
-//                var name = ""
-//                if user.displayName != nil {
-//                    name = user.displayName!
-//                    print("User displayName is \(name)")
-//                } else {
-//                    name = ""
-//                    print("User displayName is \(name)")
-//                }
-//                let email = user.email!
-//                userDict = ["uid": uid, "name": name, "email": email]
-                print("Here is userDict: \(userDict)")
-                self.welcomeTextLabel.hidden = false
-                self.welcomeTextLabel.text = "Welcome, \(userDict["name"])"
-            } else {
-                //no user is signed in
-                print("No user is signed in.")
-                self.welcomeTextLabel.text = "Please sign up or log in"
-                self.welcomeTextLabel.hidden = false
+        if self.emailTextField.text == "" {
+            print("No email entered.")
+            let anim = CAKeyframeAnimation( keyPath:"transform" )
+            anim.values = [
+                NSValue( CATransform3D:CATransform3DMakeTranslation(-10, 0, 0 ) ),
+                NSValue( CATransform3D:CATransform3DMakeTranslation( 10, 0, 0 ) )
+            ]
+            anim.autoreverses = true
+            anim.repeatCount = 2
+            anim.duration = 7/100
+            
+            self.emailTextField.layer.addAnimation( anim, forKey:nil )
+            self.welcomeTextLabel.hidden = false
+            self.welcomeTextLabel.text = "Who dis?"
+        } else
+            if self.passwordTextField.text == "" {
+            print("No password entered.")
+            let anim = CAKeyframeAnimation( keyPath:"transform" )
+            anim.values = [
+                NSValue( CATransform3D:CATransform3DMakeTranslation(-10, 0, 0 ) ),
+                NSValue( CATransform3D:CATransform3DMakeTranslation( 10, 0, 0 ) )
+            ]
+            anim.autoreverses = true
+            anim.repeatCount = 2
+            anim.duration = 7/100
+            
+            self.passwordTextField.layer.addAnimation( anim, forKey:nil )
+            self.welcomeTextLabel.hidden = false
+            self.welcomeTextLabel.text = "Oops, no password!"
+        } else {
+            print("Attempting to sign in...")
+            FIRAuth.auth()?.addAuthStateDidChangeListener { auth, user in
+                if let user = user {
+                    loggedIn = true
+                    print("loggedIn = \(loggedIn)")
+                    uid = user.uid
+                    print("Fetching user data from user child path \(uid)")
+                    self.currentUser(uid)
+                    self.performSegueWithIdentifier("logIn", sender: sender)
+                } else {
+                    //no user is signed in
+                    loggedIn = false
+                    print("loggedIn = \(loggedIn)")
+                    print("User not logged in.")
+                    let invalidUserAlert = UIAlertController(title: "Something's gone wrong", message: "Please try again. If you don't remember your password, we can reset it for you.", preferredStyle: UIAlertControllerStyle.Alert)
+                    let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+                        
+                    }
+                    let PasswordAction = UIAlertAction(title: "Reset password", style: .Cancel) { (action)
+                        in
+                        let email = self.emailTextField.text
+                        FIRAuth.auth()?.sendPasswordResetWithEmail(email!) { error in
+                            if let error = error {
+                                // An error happened.
+                            } else {
+                                // Password reset email sent.
+                                print("Password reset email sent.")
+                            }
+                        }
+                    }
+                    invalidUserAlert.addAction(OKAction)
+                    invalidUserAlert.addAction(PasswordAction)
+                    self.presentViewController(invalidUserAlert, animated: true, completion: nil)
+                    }
             }
         }
     }
-    
-    func addNewUser() {
-        self.currentDate()
-        let userData = User(uid: userDict["uid"]! as! String, email: userDict["email"]! as! String, displayName: userDict["name"]! as! String, score: 0, addedDate: dateString)
-        let userRef = ref.child("users/")
-        let newUser = userRef.child(uid)
-        // TODO: MAKE SURE NEW CHILD NODE ADDED FOR NEW USER
-        newUser.setValue(userData.toAnyObjectUser())
-        newUser.child("exclude").child("key)").setValue(true)
-        print("New user added with uid of \(uid) and email \(userDict["email"])")
-    }
-    
 }
